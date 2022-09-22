08f22a0054fd932912801af8df6d1f3eba03f0d7
Alexander Thulin <github@thulin.at>
2022-09-22 16:57:14 +0200

    Fix number changing bug

--- src/hooks/useTaxCalculations.ts
+++ src/hooks/useTaxCalculations.ts
@@ -2,14 +2,26 @@ import { useMemo, useState } from "react";
 
 export const useTaxCalculations = () => {
 	const [billAmount, setBillAmount] = useState<number | "">("");
+	const [gross, setGross] = useState<number | "">("");
+	const [net, setNet] = useState<number | "">("");
 
 	const handlers = useMemo(
 		() => ({
-			onBillChange: (value: number | "") => setBillAmount(value),
-			onGrossChange: (value: number | "") =>
-				setBillAmount(value === "" ? "" : Math.round(value * 1.3142)),
-			onNetChange: (value: number | "") =>
-				setBillAmount(value === "" ? "" : Math.round((value * 1.3142) / 0.7)),
+			onBillChange: (value: number | "") => {
+				setBillAmount(value);
+				setGross(value === "" ? "" : Math.round(value / 1.3142));
+				setNet(value === "" ? "" : Math.round((value / 1.3142) * 0.7));
+			},
+			onGrossChange: (value: number | "") => {
+				setBillAmount(value === "" ? "" : Math.round(value * 1.3142));
+				setGross(value);
+				setNet(value === "" ? "" : Math.round(value * 0.7));
+			},
+			onNetChange: (value: number | "") => {
+				setBillAmount(value === "" ? "" : Math.round((value * 1.3142) / 0.7));
+				setGross(value === "" ? "" : Math.round(value / 0.7));
+				setNet(value);
+			},
 		}),
 		[]
 	);
@@ -17,8 +29,8 @@ export const useTaxCalculations = () => {
 	return [
 		{
 			billAmount,
-			gross: billAmount === "" ? "" : Math.round(billAmount / 1.3142),
-			net: billAmount === "" ? "" : Math.round((billAmount / 1.3142) * 0.7),
+			gross,
+			net,
 			employmentFee:
 				billAmount === "" ? "" : Math.round((billAmount / 1.3142) * 0.3142),
 		},
